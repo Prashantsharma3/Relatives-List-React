4fa3e57f680ce9450a17e34621ad4f2a47a95d50
Prashant Sharma <prashantsharmaxxx1@gmail.com>
2023-02-10 21:17:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DiwaliRelativeList = () => {
+  return (
+    <ol key="relativeList">
+      <li key="relativeListItem1">Uncle John</li>
+      <li key="relativeListItem2">Aunt Mary</li>
+      <li key="relativeListItem3">Cousin Tom</li>
+      <li key="relativeListItem4">Grandma Sarah</li>
+      <li key="relativeListItem5">Grandpa David</li>
+    </ol>
+  );
+};
+
+export default DiwaliRelativeList;
