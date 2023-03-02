5c3e5fd8744ccb02dda41772ee413d08df75702b
Prashant Sharma <prashantsharmaxxx1@gmail.com>
2023-03-02 22:27:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  render() {
+    const relatives = [
+      { id: "relativeListItem1", name: "Aunt Jane" },
+      { id: "relativeListItem2", name: "Uncle John" },
+      { id: "relativeListItem3", name: "Cousin Sarah" },
+      { id: "relativeListItem4", name: "Grandma Mary" },
+      { id: "relativeListItem5", name: "Grandpa Joe" },
+    ];
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+    return (
+      <div id="main">
+        <h1>Relatives to Visit:</h1>
+        <ol key="relativeList">
+          {relatives.map((relative) => (
+            <li key={relative.id}>{relative.name}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
